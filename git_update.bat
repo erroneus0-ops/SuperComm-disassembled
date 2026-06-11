f8cc031eb1bfcb415a47425c30968d5c460f45d4
erroneus0-ops <erroneus0@gmail.com>
2026-06-11 16:54:29 -0400

    WIP: session updates

--- git_update.bat
+++ git_update.bat
@@ -6,6 +6,7 @@ git status
 echo.
 set /p MSG="Commit message (Enter for default 'WIP: session updates'): "
 if "%MSG%"=="" set MSG=WIP: session updates
+git pull --no-rebase
 git add -A
 git commit -m "%MSG%"
 git push
